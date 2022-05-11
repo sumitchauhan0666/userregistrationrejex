69905b538433f2123563bfc0f5b2ded0b036c089
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-11 23:21:45 +0530

    UC4'MOBILE UPDATE'

--- src/rejex/rejex.java
+++ src/rejex/rejex.java
@@ -7,16 +7,20 @@ public class rejex  {
 
     static Pattern pattern = Pattern.compile("^[A-Z][a-z]{2}$");
     static Pattern emailpattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    static Pattern Mobilepattern = Pattern.compile("(?:\\+?(\\d{2}))?+[\s]+[897]{1}+[0-9]{9}");
     public static void main(String[] args) {
 // 1abb23a
         String Firstname = "Ram";
         String Secondname= "San";
         String Email= "abc.dfv@bl.com";
+        String Mobilenumber="91 9033388878";
 
         Matcher Fnamematcher = pattern.matcher(Firstname);
         Matcher Snamematcher = pattern.matcher(Secondname);
         Matcher Emailmatcher = emailpattern.matcher(Email);
-        if( Fnamematcher.matches()==true && Snamematcher.matches()  && Emailmatcher.matches()==true){
+        Matcher Mobilematcher = Mobilepattern.matcher(Mobilenumber);
+
+        if(Fnamematcher.matches()==true && Snamematcher.matches()  && Emailmatcher.matches()==true  &&Mobilematcher.matches()==true ){
             System.out.println("Matches");
         }
         else {
