1fc266eea8b3e05c68c730e959609c9dad910ca5
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-12 22:23:37 +0530

    UC9'password validation All email passed'

--- src/rejex/rejex.java
+++ src/rejex/rejex.java
@@ -6,11 +6,14 @@ import java.util.regex.Pattern;
 public class rejex  {
 
     static Pattern pattern = Pattern.compile("^(?=[^A-Z]*[A-Z][^A-Z]*$)(?=.*\\W+)(?=.*\\d).{8}$");
+    static Pattern emailpattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         public static void main(String[] args) {
 // 1abb23a
         String password = "amAf@1@u";
+            String email= "abc.dfv@bl.com";
         Matcher passwordmatcher = pattern.matcher(password);
-        if(passwordmatcher.matches()==true ){
+            Matcher emailmatcher = emailpattern.matcher(email);
+        if(passwordmatcher.matches()==true && emailmatcher.matches()==true){
             System.out.println("Matches");
         }
         else {
