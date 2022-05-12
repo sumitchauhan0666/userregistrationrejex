f2a57dcd76b6f6818d6130b039389f965c714489
chauhansumit1136@gmail.com <Sumit@@1234>
2022-05-12 21:57:21 +0530

    UC6'password validation 1 upper case character'

--- src/rejex/rejex.java
+++ src/rejex/rejex.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class rejex  {
 
-    static Pattern pattern = Pattern.compile("^[a-z]{8}$");
+    static Pattern pattern = Pattern.compile("^(?=[^A-Z]*[A-Z][^A-Z]*$).{8}$");
         public static void main(String[] args) {
 // 1abb23a
-        String password = "damamkju";
+        String password = "amAfiuku";
         Matcher passwordmatcher = pattern.matcher(password);
         if(passwordmatcher.matches()==true ){
             System.out.println("Matches");
